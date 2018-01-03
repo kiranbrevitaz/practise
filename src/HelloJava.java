a012c7f1d1a0e8cad8277bdde2098016846d152e
Kiran Suthar <ksuthar@benelog.com>
2018-01-03 23:33:06 +0530

    Hello.java deleted and Hi.java created

--- src/HelloJava.java
+++
@@ -1,5 +0,0 @@
-
-
-public static void main(agrs[string]){
-    println("Hello how are you ")
-}
---
+++ src/Hi.java
@@ -0,0 +1,3 @@
+public static void main(agrs[string]){
+        println("This is hi.java file ")
+        }
